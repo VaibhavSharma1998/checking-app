53a0dd3fcafca7bf585a26669c491b615ec3ea4b
VaibhavSharma1998 <sharmavaibhav203@gmail.com>
2023-10-26 20:01:03 +0530

    qualification removed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ function App() {
     <div className="w-full px-[10%] bg-[#000] text-[#fff] flex items-center ">
       <div className="w-[50%]">
         <p className="text-center">Vaibhav</p>
-        <p>Scince</p>
+       
       </div>
       <div className="w-[50%]">
       <p className="text-center">Sharma</p>
-      <p>Graduate</p>
+     
       </div>
     </div>
   );
