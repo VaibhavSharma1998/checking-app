f658f6f1c469253ad7aa37aeda4a7585dcd17d4b
VaibhavSharma1998 <sharmavaibhav203@gmail.com>
2023-11-05 23:44:15 +0530

    "try agian this time"

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import { FcGoogle } from "react-icons/fc";
+// import { FcGoogle } from "react-icons/fc";
 
 import { useNavigate } from "react-router-dom";
 
